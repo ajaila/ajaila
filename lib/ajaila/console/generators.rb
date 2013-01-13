55c57079d38d4b702d292f721591e9f5733ecb39
Max Makarochkin <mms.post@gmail.com>
2013-01-14 01:46:09 +0400

    mac-r drank 58083 bottles of Catuaba

--- lib/ajaila/console/generators.rb
+++ lib/ajaila/console/generators.rb
@@ -30,12 +30,14 @@ command :g do |c|
         all_columns = get_columns(table)
         clast = all_columns.pop
         cleft = all_columns
-        content += "\n" + render( "_input", :columns => cleft, :last => clast, 
-                                            :collection => table.capitalize )
-        content += "\n" + render( "_output", :columns => cleft, :last => clast, 
-                                             :collection => table.capitalize )
+        content += "\n" + render( "_input", :columns => cleft, :last => clast, :collection => table.capitalize )
+        content += "\n" + render( "_output", :columns => cleft, :last => clast, :collection => table.capitalize )
       end
+    end
+    
+    if args[0] == "presenter"
       # begin
+      #    ext = "erb"
       #    using = false
       #    k, tables  = 0, []
       #    args.each do |arg|
@@ -51,9 +53,7 @@ command :g do |c|
       #    lines = []
       #    table_data = {}
       #    if tables != []
-      #      lines << "require \"mongo_mapper\"\n"
       #      tables.each do |table|
-      #        lines << "require \"./sandbox/tables/#{table}.table\"\n"
       #        File.open("sandbox/tables/#{table}.table.rb", "r") do |infile|
       #          while (line = infile.gets)
       #            clas = line[/class\s(\S+)/,1] if clas == nil
@@ -65,17 +65,26 @@ command :g do |c|
       #      end
 
       #      table_data.each_key do |table|
-      #        lines << "  #{table}.create("
+      #        lines << "<% #{table}.all.each do |d| %>\n"
       #        table_data[table].each do |key|
-      #          lines << "#{key}: #{key}, " if key != table_data[table].last
-      #          lines << "#{key}: #{key}" if key == table_data[table].last
+      #          lines << "  <div><b>#{key}</b>: <%= d.#{key} %></div>\n"
       #        end
-      #        lines << ")\n"
+      #        lines << "  <br>\n"
+      #        lines << "<% end %>"
       #      end
       #    end
 
+      #    route = []
+      #    route << "\n"
+      #    route << "get \"/#{args[1].downcase}\" do\n"
+      #    route << "  erb \"#{args[1].downcase}.presenter\".to_sym\n"
+      #    route << "end\n"
+      #    File.open("service.rb", "a") do |file|
+      #      file.puts route
+      #    end
+
       #   if using == true
-      #    puts "Ajaila: No selector title or table was selected \n( add: using file:SomeFile.csv table:SomeTable )".color(Colors::YELLOW) if files == [] or tables == [] 
+      #    puts "Ajaila: No presenter title or table was selected \n( add: using file:SomeFile.csv table:SomeTable )".color(Colors::YELLOW) if files == [] or tables == [] 
       #    raise TypeError if tables == [] 
       #   end
       #    content = lines.join
@@ -83,66 +92,7 @@ command :g do |c|
       #   raise TypeError, 'Ajaila: wrong command...'.color(Colors::RED)
       # end
     end
-    
-    if args[0] == "presenter"
-      begin
-         ext = "erb"
-         using = false
-         k, tables  = 0, []
-         args.each do |arg|
-           k += 1
-           using = true if arg == "using" and k == 3
-           next if k <= 3
-           additions = arg.split(':')
-           addition_type = additions[0].downcase
-           addition_core = additions[1].downcase
-           tables << addition_core if addition_type == "table"
-         end
-         content = ""
-         lines = []
-         table_data = {}
-         if tables != []
-           tables.each do |table|
-             File.open("sandbox/tables/#{table}.table.rb", "r") do |infile|
-               while (line = infile.gets)
-                 clas = line[/class\s(\S+)/,1] if clas == nil
-                 key = line[/key\s:(\S+),/,1]
-                 table_data[clas] = [] if clas != nil and table_data[clas] == nil
-                 table_data[clas] << key if key != nil
-               end
-             end
-           end
 
-           table_data.each_key do |table|
-             lines << "<% #{table}.all.each do |d| %>\n"
-             table_data[table].each do |key|
-               lines << "  <div><b>#{key}</b>: <%= d.#{key} %></div>\n"
-             end
-             lines << "  <br>\n"
-             lines << "<% end %>"
-           end
-         end
-
-         route = []
-         route << "\n"
-         route << "get \"/#{args[1].downcase}\" do\n"
-         route << "  erb \"#{args[1].downcase}.presenter\".to_sym\n"
-         route << "end\n"
-         File.open("service.rb", "a") do |file|
-           file.puts route
-         end
-
-        if using == true
-         puts "Ajaila: No presenter title or table was selected \n( add: using file:SomeFile.csv table:SomeTable )".color(Colors::YELLOW) if files == [] or tables == [] 
-         raise TypeError if tables == [] 
-        end
-         content = lines.join
-       rescue
-        raise TypeError, 'Ajaila: wrong command...'.color(Colors::RED)
-      end
-    end
-
-    # puts info("Generating #{args[0]} \"#{args[1]}\"")
     dir = target_dir(args)
     File.open(ROOT + "/#{dir}#{args[1].downcase}.#{args[0]}.rb", 'w') {|f| f.write(content) } if args[0] != "api"
     puts success("Generated #{args[0]} #{args[1]} successfully!")    
