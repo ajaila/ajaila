2bacbbec92318d878ec6cde04717436d87dd65ce
Max Makarochkin <mms.post@gmail.com>
2013-01-14 00:39:09 +0400

    mac-r drank 66901 bottles of brew

--- lib/ajaila/console/console_helper.rb
+++ lib/ajaila/console/console_helper.rb
@@ -1,6 +1,7 @@
 KNOWN_INSTANCES = ["miner","selector","presenter","table","api"]
 KNOWN_CLASSES = ["Array", "Float", "Hash", "Integer", "NilClass", "Object", "String", "Time", "Binary", "Boolean", "Date", "ObjectId", "Set"]
 TEMPLATES = File.expand_path( "../generator_templates", __FILE__)
+SUPPORTED_INPUTS = ["csv"]
 
 def check_inputs(args)
   raise TypeError, warning("Nothing to generate...") if args == []
@@ -16,28 +17,74 @@ def additional_params(args)
     next if k <= 2
     keys << arg
   end
-  raise TypeError, warning("\"#{args[1]}\" #{args[0]} needs\n additional parameters to be specified") if keys == []
+  raise TypeError, warning("\"#{args[1]}\" #{args[0]} needs additional parameters to be specified") if keys == []
   return keys
 end
 
 def target_dir(args)
-  dir = "datasets/" if args[0] == "selector"
-  dir = "sandbox/miners/" if args[0] == "miner"
-  dir = "sandbox/presenters/" if args[0] == "presenter"
-  dir = "sandbox/tables/" if args[0] == "table"
+  return "datasets/" if args[0] == "selector"
+  return "sandbox/miners/" if args[0] == "miner"
+  return "sandbox/presenters/" if args[0] == "presenter"
+  return "sandbox/tables/" if args[0] == "table"
 end
 
 def parse_columns(columns)
   pairs = []
   columns.each do |key_string|
     params = key_string.split(':')
-    raise TypeError, warning("Wrong format of table parameters\n(name:String/Integer/Date/etc)") if KNOWN_CLASSES.include?(params[1].capitalize) == false
+    raise TypeError, warning("Wrong format of table parameters (name:String/Integer/Date/etc)") if KNOWN_CLASSES.include?(params[1].capitalize) == false
     pairs << [params[0].downcase, params[1].capitalize]
   end
   return pairs
 end
 
+def get_tables(params)
+  tables = []
+  params.each do |param|
+  	p = param.split(":")
+  	next if p.first != "table"
+    tables << p[1] if p[0] == "table"
+  end
+  raise TypeError, warning("No tables included. Add: \"table:SomeTable\"") if tables == []
+  return tables
+end
+
+def get_files(params)
+  files = []
+  params.each do |param|
+  	p = param.split(":")
+  	next if p.first != "file"
+  	raise TypeError, warning("Sorry, but extension of #{p[1]} is not supported") if SUPPORTED_INPUTS.include?(p[1].split(".")[1].downcase) == false
+    files << p[1] if p[0] == "file"
+  end
+  raise TypeError, warning("No file included. Add: \"file:file_name.csv\"") if files == []
+  return files
+end
+
+def get_file(params)
+  files = get_files(params)
+  raise TypeError, warning("Only one file can be included") if files.length > 1
+  return files[0]
+end
+
+def get_table(params)
+  tables = get_tables(params)
+  raise TypeError, warning("Only one table can be included") if tables.length > 1
+  return tables[0]
+end
+
 def render(template, *params)
-  puts *params
   Tilt.new(TEMPLATES + "/" + template + ".liquid").render(nil, *params)
+end
+
+def get_columns(table)
+  columns = []
+  File.open(ROOT+"/sandbox/tables/#{table.downcase}.table.rb", "r") do |infile|
+    while (line = infile.gets)
+      key = line[/key\s:(\S+),/,1]
+      columns << key if key != nil
+    end
+  end
+  raise TypeError, warning("Could not open \"#{table}\". There is something wrong with it.") if columns == []
+  return columns
 end
--- lib/ajaila/console/generators.rb
+++ lib/ajaila/console/generators.rb
@@ -1,94 +1,26 @@
 # generating miners,selectors,tables and presenters 
 command :g do |c|
   c.action do |global_options,options,args|
+
     check_inputs(args)
- 
+    content = ""
     if args[0] == "table"
-        columns = additional_params(args)
-        collection = args[1].capitalize
-        key_pairs = parse_columns(columns)
-        content = render("table", :collection => collection, :keys => key_pairs)
+      columns = additional_params(args)
+      collection = args[1].capitalize
+      key_pairs = parse_columns(columns)
+      content = render("table", :collection => collection, :keys => key_pairs)
     end
 
-
     if args[0] == "selector"
-      begin
-         using = false
-         k, files, tables  = 0, [], []
-         args.each do |arg|
-           k += 1
-           using = true if arg == "using" and k == 3
-           next if k <= 3
-           additions = arg.split(':')
-           addition_type = additions[0].downcase
-           addition_core = additions[1].downcase
-           files << addition_core if addition_type == "file"
-           tables << addition_core if addition_type == "table"
-         end
-         content = ""
-         puts 'Ajaila: wrong parameters usage (ex. using file:SomeFile.csv)'.color(Colors::YELLOW) if using == true and files.empty?
-         raise TypeError if using == true and files.empty?
-         supported_files = ["csv"]
-         lines = []
-         table_data = {}
-         if tables != []
-           lines << "require \"csv\"\n"
-           lines << "require \"mongo_mapper\"\n"
-           tables.each do |table|
-             lines << "require \"./sandbox/tables/#{table}.table\"\n"
-             File.open("sandbox/tables/#{table}.table.rb", "r") do |infile|
-               while (line = infile.gets)
-                 clas = line[/class\s(\S+)/,1] if clas == nil
-                 key = line[/key\s:(\S+),/,1]
-                 table_data[clas] = [] if clas != nil and table_data[clas] == nil
-                 table_data[clas] << key if key != nil
-               end
-             end
-           end
-         end
-
-         if files != []
-           files.each do |key_string|
-             file = key_string.split('.')
-             file_name = file[0]
-             file_type = file[1]
-             puts 'Ajaila: sorry, but we support only .csv for now...'.color(Colors::YELLOW) if supported_files.include?(file_type) == false or file_type == nil
-             raise TypeError if supported_files.include?(file_type.to_s) == false or file_type == nil
-             if file_type == "csv"
-               cs = 1 if cs == nil
-               kf = "qwerty".split("").shuffle.join.upcase
-               lines << "require \"csv\"\n" if cs == nil
-               lines << "#{kf} = File.expand_path(\"./datasets/raw/#{file_name.downcase}.#{file_type.downcase}\")\n\n"
-               lines << "CSV.foreach(#{kf}) do |row|\n"
-               table_data.each_key do |table|
-                 table_data[table].each do |key|
-                   lines <<  "  #{key} = row[0] # put other indexes instead of 0\n"
-                 end
-               end
-               if tables != []
-                 table_data.each_key do |table|
-                   lines << "  #{table}.create("
-                   table_data[table].each do |key|
-                     lines << "#{key}: #{key}, " if key != table_data[table].last
-                     lines << "#{key}: #{key}" if key == table_data[table].last
-                   end
-                   lines << ")\n"
-                 end
-               end
-               lines << "end\n\n"
-             end
-           end
-         end
-
-
-        if using == true
-         puts "Ajaila: No selector title, file or table was selected \n( add: using file:SomeFile.csv table:SomeTable )".color(Colors::YELLOW) if files == [] or tables == [] 
-         raise TypeError if files == [] or tables == [] 
-        end
-         content = lines.join
-       rescue
-        raise TypeError, 'Ajaila: wrong command...'.color(Colors::RED)
-      end
+      content = render("selector")
+      io = additional_params(args)
+      file = get_file(io)
+      file_name = get_file(io).split(".").first.upcase
+      table = get_table(io)
+      all_columns = get_columns(table)
+      clast = all_columns.pop
+      cleft = all_columns
+      content += "\n" + render( "_parser", :columns => cleft, :last => clast, :collection => table.capitalize, :file => file, :file_name => file_name )
     end
 
     if args[0] == "miner"
@@ -254,11 +186,9 @@ command :g do |c|
         raise TypeError, 'Ajaila: wrong command...'.color(Colors::RED)
       end
     end
-    puts info("Generating #{args[0]} \"#{args[1]}\"")
+    # puts info("Generating #{args[0]} \"#{args[1]}\"")
     dir = target_dir(args)
     File.open(ROOT + "/#{dir}#{args[1].downcase}.#{args[0]}.rb", 'w') {|f| f.write(content) } if args[0] != "api"
-    puts "\tGenerated #{args[0]} #{args[1]} successfully!".color(Colors::GREEN)
-    
+    puts success("Generated #{args[0]} #{args[1]} successfully!")    
   end
-
 end
