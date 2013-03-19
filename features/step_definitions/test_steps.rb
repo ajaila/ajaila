648b3dd4b01e1cfc1024cc3465981a3e4b3de64c
mac-r <mms.post@gmail.com>
2013-03-20 01:08:08 +0400

    removed cucumber

--- features/step_definitions/test_steps.rb
+++
@@ -1,9 +0,0 @@
-CONTENTS = 'some contents'
-
-# Given /^the file "([^"]*)" exists$/ do |file|
-#   File.open(file,'w') { |file| file.puts(CONTENTS) }
-# end
-# Then /^the file "([^"]*)" should the same contents as "([^"]*)"$/ do |dest_file,source_file|
-#   contents = File.read(dest_file)
-#   contents.should == CONTENTS
-# end
